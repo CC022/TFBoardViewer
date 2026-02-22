3ef96ecf814dbb60d8850ea18f143fcd683b57b5
kojimaZZ <zzc9x@icloud.com>
2026-02-21 16:12:21 -0800

    Initial Commit

---
+++ TFBoardViewer/TFBoardViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFBoardViewerApp.swift
+//  TFBoardViewer
+//
+//  Created by zzc on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TFBoardViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
